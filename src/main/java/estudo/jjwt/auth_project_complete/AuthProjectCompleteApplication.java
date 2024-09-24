62f8bb01cfa3e8697ab03e4fcdc54cfaaf5c9312
Lucas <lucas.hen.lps@gmail.com>
2024-09-24 17:16:32 -0300

    feat: initial setup

---
+++ src/main/java/estudo/jjwt/auth_project_complete/AuthProjectCompleteApplication.java
@@ -0,0 +1,13 @@
+package estudo.jjwt.auth_project_complete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthProjectCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthProjectCompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudo/jjwt/auth_project_complete/AuthProjectCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.jjwt.auth_project_complete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
