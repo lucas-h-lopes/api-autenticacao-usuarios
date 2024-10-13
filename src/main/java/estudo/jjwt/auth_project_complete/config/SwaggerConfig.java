adba033ef1beaa9825504278d812f48d86dee05d
Lucas <lucas.hen.lps@gmail.com>
2024-10-12 21:22:21 -0300

    feat: Swagger authentication with SecurityScheme

--- src/main/java/estudo/jjwt/auth_project_complete/config/SwaggerConfig.java
+++ src/main/java/estudo/jjwt/auth_project_complete/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package estudo.jjwt.auth_project_complete.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +13,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI(){
-        return new OpenAPI().info(new Info()
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(new Info()
                 .title("API Autenticação de Usuários")
                 .contact(new Contact() .email("lucas.hen.lps@gmail.com")
                         .name("Lucas Lopes")
                         .url("https://www.linkedin.com/in/lucashlopes/"))
                         .description("Uma API para autenticação e manipulação de usuários utilizando a biblioteca [JJWT](https://github.com/jwtk/jjwt). ")
                 .version("v2"));
+    }
+
+    private SecurityScheme securityScheme(){
+        return new SecurityScheme()
+                .description("Provide a valid token to authenticate")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("security");
     }
 }
